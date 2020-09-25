d8c4025448f6c91075938ade27b08fd6dc621a61
T. Xavier Newkirk <xnewkirk13@gmail.com>
2020-09-25 14:50:26 -0400

    email-app

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Email em1 = new Email("John", "Smith");
+    System.out.println(em1.showInfo());
+    }
+}
